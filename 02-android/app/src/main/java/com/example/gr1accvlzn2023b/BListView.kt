3f0444fbfe0b39b9e6ce0e769f537ee5234d5e7d
suroraFi <diana0990@hotmail.com>
2023-12-25 16:24:20 -0500

    anadir entrenador

--- 02-android/app/src/main/java/com/example/gr1accvlzn2023b/BListView.kt
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/BListView.kt
@@ -2,10 +2,45 @@ package com.example.gr1accvlzn2023b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
 class BListView : AppCompatActivity() {
+    val arreglo = BBaseDatosMemoria.arregloBEntrenador
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this, // Contexto
+            // como se va a ver (XML)
+            android.R.layout.simple_list_item_1,
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+
+        val botonAnadirListView = findViewById<Button>(
+            R.id.btn_anadir_list_view)
+        botonAnadirListView
+            .setOnClickListener {
+                anadirEntrenador(adaptador)
+            }
     }
+    fun anadirEntrenador(
+        adaptador: ArrayAdapter<BEntrenador>
+    ){
+        arreglo.add(BEntrenador(1, "Sebastian", "Descripcion"))
+        adaptador.notifyDataSetChanged()
+    }
+
+
+
+
+
+
+
+
 }
