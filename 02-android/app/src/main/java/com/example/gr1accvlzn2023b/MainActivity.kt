d568719716023faab1e39fba4676390a574537ae
suroraFi <diana0990@hotmail.com>
2023-12-25 14:24:40 -0500

    ciclo de vida

--- 02-android/app/src/main/java/com/example/gr1accvlzn2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.gr1accvlzn2023b
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
+
+
 }
