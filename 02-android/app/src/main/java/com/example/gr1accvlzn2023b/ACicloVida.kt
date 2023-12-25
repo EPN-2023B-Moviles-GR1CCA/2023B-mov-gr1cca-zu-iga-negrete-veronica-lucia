85b4644ef3534cbbb5c336da72b8122da2d7b169
suroraFi <diana0990@hotmail.com>
2023-12-25 14:24:23 -0500

    ciclo de vida

---
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/ACicloVida.kt
@@ -0,0 +1,11 @@
+package com.example.gr1accvlzn2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+}
