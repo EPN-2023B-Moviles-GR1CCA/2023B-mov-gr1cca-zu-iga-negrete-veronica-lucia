9f36d88608f8d1be99e09eade8022befdd758c5e
suroraFi <diana0990@hotmail.com>
2023-12-25 15:26:35 -0500

    codigo de modelo y memoria

--- 02-android/app/src/main/java/com/example/gr1accvlzn2023b/BEntrenador.kt
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/BEntrenador.kt
@@ -1,4 +1,11 @@
 package com.example.gr1accvlzn2023b
 
-class BEntrenador {
+class BEntrenador(
+    var id: Int,
+    var nombre: String?,
+    var descripcion: String?
+) {
+    override fun toString(): String {
+        return "${nombre} -  ${descripcion}"
+    }
 }
