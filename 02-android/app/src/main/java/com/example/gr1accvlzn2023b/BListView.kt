be92364b97b00ac5e4ba83c3129fa093100541ed
suroraFi <diana0990@hotmail.com>
2023-12-25 15:11:51 -0500

    actvidad list view

---
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/BListView.kt
@@ -0,0 +1,11 @@
+package com.example.gr1accvlzn2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blist_view)
+    }
+}
