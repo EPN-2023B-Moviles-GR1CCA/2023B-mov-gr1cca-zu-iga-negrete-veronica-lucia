dbde848dc629e262c8b4c294f75897f780218b75
suroraFi <diana0990@hotmail.com>
2024-01-04 17:28:58 -0500

    funciones crear y eliminar entrenador

--- 02-android/app/src/main/java/com/example/gr1accvlzn2023b/ESqliteHelperEntrenador.kt
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/ESqliteHelperEntrenador.kt
@@ -1,4 +1,73 @@
 package com.example.gr1accvlzn2023b
 
-class ESqliteHelperEntrenador {
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ESqliteHelperEntrenador(
+    contexto: Context?,
+) : SQLiteOpenHelper(
+    contexto,
+    "moviles",
+    null,
+    1
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        val scriptSQLCrearTablaEntrenador =
+            """
+                CREATE TABLE ENTRENADOR(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre VARCHAR(50),
+                descripcion VARCHAR(50)
+                )
+            """.trimIndent()
+        db?.execSQL(scriptSQLCrearTablaEntrenador)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+
+    fun crearEntrenador(nombre: String, descripcion: String): Boolean{
+        val basedatosEscritura = writableDatabase
+        val valoresAGuardar = ContentValues()
+        valoresAGuardar.put("nombre", nombre)
+        valoresAGuardar.put("descripcion", descripcion)
+        val resultadoGuardar = basedatosEscritura
+            .insert(
+                "ENTRENADOR",
+                null,
+                valoresAGuardar
+            )
+        basedatosEscritura.close()
+        return if(resultadoGuardar.toInt() == -1) false else true
+    }
+
+    fun eliminarEntrenadorFormulario(id: Int):Boolean{
+        val conexionEscritura = writableDatabase
+        val parametrosConsultaDelete = arrayOf(id.toString())
+        val resultadoEliminacion = conexionEscritura
+            .delete(
+                "ENTRENADOR",
+                "id=?",
+                parametrosConsultaDelete
+            )
+        conexionEscritura.close()
+        return if(resultadoEliminacion == -1) false else true
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- 02-android/app/src/main/java/com/example/gr1accvlzn2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/MainActivity.kt
@@ -93,6 +93,11 @@ class MainActivity : AppCompatActivity() {
                 abrirActividadConParametros(
                     CIntentExplicitoParametros::class.java)
             }
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
     } // Termina onCreate
     fun abrirActividadConParametros(
         clase: Class<*>
