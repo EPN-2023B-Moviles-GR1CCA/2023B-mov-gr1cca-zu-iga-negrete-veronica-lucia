f703bde417cf0e91dac9c5b1c2bb7d18bf5b54af
suroraFi <diana0990@hotmail.com>
2024-01-10 21:02:05 -0500

    main activity

---
+++ Examen01/app/src/main/java/com/example/examen01/NuevoDispositivo.kt
@@ -0,0 +1,11 @@
+package com.example.examen01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevoDispositivo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_dispositivo)
+    }
+}
