c69a0dc039fc790348a37f0b3e73c46f7343dcd4
suroraFi <diana0990@hotmail.com>
2024-01-10 23:03:58 -0500

    hasta funcion de eliminar

--- Examen01/app/src/main/java/com/example/examen01/ListaDispositivos.kt
+++ Examen01/app/src/main/java/com/example/examen01/ListaDispositivos.kt
@@ -1,14 +1,32 @@
 package com.example.examen01
 
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 
 class ListaDispositivos : AppCompatActivity() {
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (result.data != null) {
+                    // Logica Negocio
+                    val data = result.data
+                }
+            }
+        }
     val arreglo = BaseDatosDispositivo.arregloBDispositivo
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +34,8 @@ class ListaDispositivos : AppCompatActivity() {
         setContentView(R.layout.activity_lista_dispositivos)
 
         val listView = findViewById<ListView>(R.id.lv_lista_disp)
-        val adaptador = ArrayAdapter(this, // Contexto
+        val adaptador = ArrayAdapter(
+            this, // Contexto
             // como se va a ver (XML)
             android.R.layout.simple_list_item_1,
             arreglo
@@ -28,10 +47,18 @@ class ListaDispositivos : AppCompatActivity() {
         registerForContextMenu(listView)
 
 
+        val botonCrear = findViewById<Button>(R.id.btn_nuevo_disp)
+        botonCrear.setOnClickListener { irActividad(NuevoDispositivo::class.java) }
+
 
     }
+
     var posicionItemSeleccionado = 0
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
         super.onCreateContextMenu(menu, v, menuInfo)
         // Llenamos las opciones del menu
         val inflater = menuInflater
@@ -42,16 +69,68 @@ class ListaDispositivos : AppCompatActivity() {
         posicionItemSeleccionado = posicion
     }
 
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.mi_editar -> {
+                abrirActividadConParametros(EditarDispositivo::class.java, posicionItemSeleccionado)
+                return true
+            }
 
+            R.id.mi_eliminar -> {
+                abrirDialogo(posicionItemSeleccionado)
+                return true
+            }
 
+            R.id.mi_verPiezas -> {
+                abrirActividadConParametros(ListaPiezas::class.java, posicionItemSeleccionado)
+                return true
+            }
 
+            else -> super.onContextItemSelected(item)
+        }
+    }
 
+    fun abrirDialogo(posicionDispositivo: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("¿Esta seguro que desea eliminar?")
+        builder.setPositiveButton("Aceptar",
+            DialogInterface.OnClickListener { dialog, which ->
+                val id = posicionDispositivo + 1
+                eliminarDispositivo(id)
+                dialog.dismiss()
+                irActividad(ListaDispositivos::class.java)
 
+            }
+        )
 
+        builder.setNegativeButton("Cancelar", null)
+        val dialogo = builder.create()
+        dialogo.show()
+    }
+    fun eliminarDispositivo(id: Int){
+        // Buscar el dispositivo a eliminar
+        val dispositivoAEliminar = arreglo.find { it.idDispositivo == id }
+        // Si se encontró el dispositivo
+        if (dispositivoAEliminar != null) {
+            // Eliminar el dispositivo del arreglo
+            arreglo.remove(dispositivoAEliminar)
+        } else {
+            // No se encontró el dispositivo
+            println("No se encontró un dispositivo con el ID $id.")
+        }
+    }
 
+    fun irActividad(clase: Class<*>) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
 
+    fun abrirActividadConParametros(clase: Class<*>, dispositivoEditar: Int) {
+        val intentExplicito = Intent(this, clase)
+        // Enviar parametros (solamente variables primitivas)
+        intentExplicito.putExtra("id", dispositivoEditar)
 
-
-
+        callbackContenidoIntentExplicito.launch(intentExplicito)
+    }
 
 }
