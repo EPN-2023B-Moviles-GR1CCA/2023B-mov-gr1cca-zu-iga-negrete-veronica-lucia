b113baa06c1fe4003be5a973db2dc2caee4179d5
suroraFi <diana0990@hotmail.com>
2024-01-30 18:17:06 -0500

    deber 02

---
+++ 02-android/app/src/main/java/com/example/gr1accvlzn2023b/GGoogleMapsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gr1accvlzn2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GGoogleMapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ggoogle_maps)
+    }
+}
